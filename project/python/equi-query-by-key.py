a12f1ff8bc8730196ebdd8f8852a6d04f974f086
Israel Gonzalez <40414620+igonzals@users.noreply.github.com>
2024-11-09 13:11:00 -0700

    simplifying comments

--- project/python/equi-query-by-key.py
+++ project/python/equi-query-by-key.py
@@ -7,26 +7,23 @@ def queries_by_key (num_queries):
 
   print(f"Average runtime of {num_queries:,} queries:")
 
-  runtimes = []
   customer_id = random.randint(1, 100)
 
+  # Redis
+  runtimes = []
   for i in range(1, num_queries):
-    # Connect to Redis
     r = redis.StrictRedis(host='localhost', port=6379)
     start_time = time.time()
     first_name = r.hget("customer:" + str(customer_id), "first_name")
     end_time = time.time()
     duration = end_time - start_time
     runtimes.append(duration)
-    # Close the connection (to prevent memory caching)
     r.close()
-  # Print the average runtime formatted to 5 decimal places
   print(f"  Average Runtime: {sum(runtimes) / len(runtimes):.5f} seconds in Redis")
 
+  # SQLite
   runtimes = []
-
   for i in range(1, num_queries):
-    # Connect to SQLite database
     conn = sqlite3.connect('../data/sakila.db')
     cursor = conn.cursor()
     start_time = time.time()
@@ -35,16 +32,12 @@ def queries_by_key (num_queries):
     end_time = time.time()
     duration = end_time - start_time
     runtimes.append(duration)
-    # Close the connection (to prevent memory caching)
     conn.close()
-
-  # Print the average runtime formatted to 5 decimal places
   print(f"  Average Runtime: {sum(runtimes) / len(runtimes):.5f} seconds in SQLite")
-  print()
 
 print()
 print("Redis vs SQLite")
 print("Equi queries by key")
 print("===================\n")
-
-queries_by_key(10000)
+queries_by_key(1000)
+print()
