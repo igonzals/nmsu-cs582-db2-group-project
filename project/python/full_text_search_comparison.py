376ac6d9f2e15067a2b68695cf25fb6b01b434b9
Israel Gonzalez <40414620+igonzals@users.noreply.github.com>
2024-11-10 22:23:53 -0700

    Full text search query logs and fixes db path

--- project/python/full_text_search_comparison.py
+++ project/python/full_text_search_comparison.py
@@ -28,7 +28,7 @@ def redis_full_text_search(query_text):
     return result
 
 # SQLite  data setting
-conn = sqlite3.connect('/data/sakila.db')  
+conn = sqlite3.connect('../data/sakila.db')  
 cursor = conn.cursor()
 
 # Create FTS5  insert data
@@ -52,23 +52,21 @@ def measure_query_time(system, query_function, query_text, num_iterations):
         runtimes.append(duration)
     return sum(runtimes) / len(runtimes)
 
- # Get execution time in format yyyy-mmm-dd hh:mm:ss
-execution_group_time = time.strftime("%Y-%b-%d %H:%M:%S")
+print("Testing Redis and SQLite query times with increasing iteration counts:\n")
 
 # Test query time with increasing query counts 1 to 1000
 query_counts = [10, 100, 1000]   
 
-print("Testing Redis and SQLite query times with increasing iteration counts:\n")
-
-for count in query_counts:
-    redis_time = measure_query_time("Redis", redis_full_text_search, query_text, count)
-    sqlite_time = measure_query_time("SQLite", sqlite_full_text_search, query_text, count)
-    print(f"For {count} queries:")
+for num_queries in query_counts:
+    execution_group_time = time.strftime("%Y-%b-%d %H:%M:%S")
+    redis_time = measure_query_time("Redis", redis_full_text_search, query_text, num_queries)
+    sqlite_time = measure_query_time("SQLite", sqlite_full_text_search, query_text, num_queries)
+    print(f"For {num_queries} queries:")
     print(f"  Redis Average Query Time: {redis_time:.5f} seconds")
     print(f"  SQLite Average Query Time: {sqlite_time:.5f} seconds\n")
     with open("../data/runtimes-group.tsv", "a") as f:
-        f.write(f"{execution_group_time}\tFull-text Search\tRedis\t{redis_time}\n")
-        f.write(f"{execution_group_time}\tFull-text Search\tSQLite\t{sqlite_time}\n")
+        f.write(f"{execution_group_time}\tRedis\tFull-text\t{num_queries}\t{redis_time:.5f}\n")
+        f.write(f"{execution_group_time}\tSQLite\tFull-text\t{num_queries}\t{sqlite_time:.5f}\n")
 
 # Close SQLite connection
 conn.close()
